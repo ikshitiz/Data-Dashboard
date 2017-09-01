4fb1c1c3445a564e870ad39d7a31b84673022a3e
Kshitiz <Kshtjkmr35@gmail.com>
2017-09-01 18:15:24 +0530

    remove files

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
-
 from flask import Flask, Response, json, jsonify, request
 from flask_bootstrap import Bootstrap
 from flask import render_template
 import csv
 
 
-
 app = Flask(__name__)
 Bootstrap(app)
 
-def get_csv():
-    csv_path = './static/assets/data/asansol.csv'
+def get_csv(st):
+    csv_path = st
     csv_file = open(csv_path, 'r')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
     return csv_list
 
-
-
 @app.route("/", methods = ['GET', 'POST'])
-def index():
+def index1():
     template = 'index.html'
-    object_list = get_csv()
+    object_list = get_csv('./static/assets/data/asansol.csv')
     return render_template(template, object_list=object_list)
 
+
+@app.route("/asansol", methods = ['GET', 'POST'])
+def index2():
+    template = 'index.html'
+    object_list = get_csv('./static/assets/data/asansol.csv')
+    return render_template(template, object_list=object_list)
+
+
+@app.route("/durgapur", methods = ['GET', 'POST'])
+def index3():
+    template = 'index.html'
+    object_list = get_csv('./static/assets/data/asansol.csv')
+    return render_template(template, object_list=object_list)
 
 
 if __name__ == "__main__":
